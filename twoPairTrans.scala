46bb6f8f90404d006d4c86b6a0c5025a832f3b2d
Vikram Elango <vikrame@gmail.com>
2015-01-22 13:45:30 -0500

    Update twoPairTrans.scala

--- twoPairTrans.scala
+++ twoPairTrans.scala
@@ -20,12 +20,12 @@ val myEmp = sc.textFile("/tmp/emp.txt")
 
 val myDep = sc.textFile("/tmp/dep.txt")
 
- val myEmpPair = myEmp.map(x=>(x.split(" ")(5),x))
- //res5: Array[(String, String)] = Array((1000,100 Vikram Elango 10000 2015-01-22 1000), 
- //(1001,200 Amanada clarke 10000 2015-01-22 1001), (1002,300 Emily Thorne 10000 2015-01-22 1002),
- //(1003,400 David Clarke 10000 2015-01-22 1003), (1000,500 Daniel Grayson 10000 2015-01-22 1000),
- //(1001,600 Conrad Grayson 10000 2015-01-22 1001), (1002,700 Nolan Ross 10000 2015-01-22 1002),
- //(1003,800 Jack Porter 10000 2015-01-22 1003), (1004,900 Charlotte Clarke 10000 2015-01-22 1004))
+val myEmpPair = myEmp.map(x=>(x.split(" ")(5),x))
+//res5: Array[(String, String)] = Array((1000,100 Vikram Elango 10000 2015-01-22 1000), 
+//(1001,200 Amanada clarke 10000 2015-01-22 1001), (1002,300 Emily Thorne 10000 2015-01-22 1002),
+//(1003,400 David Clarke 10000 2015-01-22 1003), (1000,500 Daniel Grayson 10000 2015-01-22 1000),
+//(1001,600 Conrad Grayson 10000 2015-01-22 1001), (1002,700 Nolan Ross 10000 2015-01-22 1002),
+//(1003,800 Jack Porter 10000 2015-01-22 1003), (1004,900 Charlotte Clarke 10000 2015-01-22 1004))
 
 val myDepPair = myDep.map((x=>(x.split(" ")(0),x))
 //res6: Array[(String, String)] = Array((1000,1000 Computer), (1001,1001 Geek), (1002,1002 Hacker),
@@ -37,5 +37,11 @@ val mySub = myEmpPair.subtractByKey(myDepPair)
 val mySub1 = myDepPair.subtractByKey(myEmpPair)
 //res8: Array[(String, String)] = Array((1005,1005 Student))
 
+val myIJoin = myEmpPair.join(myDepPair)
+//res9: Array[(String, (String, String))] = 
+//Array((1001,(200 Amanada clarke 10000 2015-01-22 1001,1001 Geek)), (1001,(600 Conrad Grayson 10000 2015-01-22 1001,1001 Geek)), 
+//(1003,(400 David Clarke 10000 2015-01-22 1003,1003 Bartender)), (1003,(800 Jack Porter 10000 2015-01-22 1003,1003 Bartender)), 
+//(1002,(300 Emily Thorne 10000 2015-01-22 1002,1002 Hacker)), (1002,(700 Nolan Ross 10000 2015-01-22 1002,1002 Hacker)), 
+//(1000,(100 Vikram Elango 10000 2015-01-22 1000,1000 Computer)), (1000,(500 Daniel Grayson 10000 2015-01-22 1000,1000 Computer)))
 
 
